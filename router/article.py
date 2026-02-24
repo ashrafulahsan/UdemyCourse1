8f0839cfaa15aee05f1f4bd47df470277cf97ba9
ashraful <ahsan.ashraful@adndiginet.com>
2026-02-24 16:34:12 +0600

    Securing endpoint using auth2: username, password

--- router/article.py
+++ router/article.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from db import db_article
 from db.database import get_db
 from schemas import ArticleBase, ArticleDisplay
+from auth.oauth2 import oauth2_scheme
 
 router = APIRouter(
     prefix="/article",
@@ -17,7 +18,7 @@ def create_article(request: ArticleBase, db: Session = Depends(get_db)):
 
 # Get Article by ID
 @router.get("/{id}", response_model=ArticleDisplay)
-def get_article(id: int, db: Session = Depends(get_db)):
+def get_article(id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     article = db_article.get_article(db, id)
     if not article:
         raise HTTPException(status_code=404, detail="Article not found")
