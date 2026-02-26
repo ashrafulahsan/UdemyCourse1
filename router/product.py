6287597a5bdee668bada67a4af3b7e850ad4415c
ashraful <ahsan.ashraful@adndiginet.com>
2026-02-27 05:39:39 +0600

    Logging

--- router/product.py
+++ router/product.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Cookie, Form, Header
 from fastapi.responses import Response, HTMLResponse, PlainTextResponse
 from typing import Optional, List
+from custom_log import log
 
 router = APIRouter(
     prefix="/products", 
@@ -24,6 +25,7 @@ def create_product(
 
 @router.get("/all")
 def get_all_products():
+    log("INFO", "Fetching all products")
     data = " ".join([product["name"] for product in products])
     response = Response(content=data, media_type="text/plain")
     response.set_cookie(key="test_cookie", value="test_cookie_value")
